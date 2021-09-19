4ec12cc41b1aeea6747c988e403edf0525d7bc60
soohyoen <gnejfejf789@naver.com>
2021-09-19 15:33:55 +0900

    small fix

--- BackEnd/articleapp/urls.py
+++ BackEnd/articleapp/urls.py
@@ -9,6 +9,7 @@ app_name = 'articleapp'
 
 urlpatterns = [
     path('<str:category>/list/', ArticleListView.as_view(), name='list'),
+
     path('create/', ArticleCreateView.as_view(), name='create'),
     path('detail/<int:pk>', ArticleDetailView.as_view(), name='detail'),
     path('update/<int:pk>', ArticleUpdateView.as_view(), name='update'),
